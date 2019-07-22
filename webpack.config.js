fb0a06f89289eb117929d7a44efda81ecc9f91b3
Karol Sitarz <ka.sitarz@gmail.com>
2019-07-22 18:48:16 +0200

    add brotli and gzip compression

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BrotliPlugin = require("brotli-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = (env, argv) => [
   {
@@ -32,14 +35,43 @@ module.exports = (env, argv) => [
     },
     plugins: [
       argv.mode === "production" ? undefined : new webpack.NamedModulesPlugin(),
-
       new HtmlWebPackPlugin({
         template: "./src/index.html",
         filename: "./index.html"
       }),
       new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ["bundle.*.js", "bundle.js"]
-      })
+        cleanOnceBeforeBuildPatterns: [
+          "bundle.*.js",
+          "bundle.*.js.br",
+          "bundle.*.js.gz",
+          "bundle.js",
+          "bundle.js.br",
+          "bundle.js.gz"
+        ]
+      }),
+      argv.mode === "development"
+        ? undefined
+        : new CompressionPlugin({
+            filename: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+          }),
+      argv.mode === "development"
+        ? undefined
+        : new BrotliPlugin({
+            asset: "[path].br[query]",
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8
+          }),
+      argv.mode === "development"
+        ? undefined
+        : new SWPrecacheWebpackPlugin({
+            cacheId: require("./package.json").name,
+            minify: true
+          })
     ].filter(Boolean),
     mode: "development",
     devtool:
